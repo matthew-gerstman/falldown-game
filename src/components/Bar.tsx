1fb4cb9562d57e66631fcf51fa62c49179a0c34e
Matthew Gerstman <mattgerstman@gmail.com>
2025-12-09 01:47:17 +0000

    Add Bar component with gap rendering

---
+++ src/components/Bar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Bar as BarType, GAME_CONFIG } from '../types/game';
+
+interface BarProps {
+  bar: BarType;
+}
+
+export const Bar: React.FC<BarProps> = ({ bar }) => {
+  const { CANVAS_WIDTH } = GAME_CONFIG;
+  
+  return (
+    <>
+      {/* Left section of bar */}
+      <div
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: bar.y,
+          width: bar.gapPosition,
+          height: bar.height,
+          backgroundColor: '#2196F3',
+          boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
+        }}
+      />
+      {/* Right section of bar */}
+      <div
+        style={{
+          position: 'absolute',
+          left: bar.gapPosition + bar.gapWidth,
+          top: bar.y,
+          width: CANVAS_WIDTH - (bar.gapPosition + bar.gapWidth),
+          height: bar.height,
+          backgroundColor: '#2196F3',
+          boxShadow: '0 2px 4px rgba(0,0,0,0.2)',
+        }}
+      />
+    </>
+  );
+};
